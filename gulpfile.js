ec574708ed8790a32c23b594ad39216d6c8d72a5
Sébastien Robaszkiewicz <sebastien.robaszkiewicz@gmail.com>
2014-12-01 17:14:57 +0100

    Updated gulpfile + reintroduced server index

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,22 @@ var rename = require('gulp-rename');
 var watch = require('gulp-watch');
 
 gulp.task('transpile', function() {
-//  return gulp.src('**/*.es6.js')
-  return gulp.src('server/ServerConnectionManager.es6.js')
+ gulp.src(['*/*.es6.js', '!node_modules/**/*.es6.js'])
     .pipe(es6({
       disallowUnknownReferences: false
     }))
     .pipe(rename(function(path) {
       path.basename = path.basename.replace('.es6', '');
     }))
+    .on('error', function(err) {
+      console.log(err.message);
+      this.end();
+    })
     .pipe(gulp.dest('.'));
 });
 
 gulp.task('watch', function() {
-  return gulp.watch('**/*.es6.js', ['transpile']);
+  gulp.watch('**/*.es6.js', ['transpile']);
 });
 
 gulp.task('default', ['transpile', 'watch']);
