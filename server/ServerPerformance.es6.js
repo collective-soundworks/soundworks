f4c4636744a1f3bf0859ad1864c8810f15af9b1a
Norbert Schnell <Norbert.Schnell@ircam.fr>
2014-12-17 09:55:35 +0100

    removed manager from performance

--- server/ServerPerformance.es6.js
+++ server/ServerPerformance.es6.js
@@ -8,17 +8,9 @@ var ioServer = require('./ioServer');
 
 class ServerPerformance {
   constructor() {
-    this.__manager = null;
+
   }
 
-  set manager(manager) {
-    this.__manager = manager;
-  }
-
-  get manager() {
-    return this.__manager;
-  }
-  
   connect(socket, player) {
 
   }
