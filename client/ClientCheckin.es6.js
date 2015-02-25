60800b52238d0a7e99e5f63329a529a274c06c31
Norbert Schnell <Norbert.Schnell@ircam.fr>
2015-02-25 23:34:42 +0100

    - renamed "display" option to "dialog" - renamed lastModule to prevModule in serial module

--- client/ClientCheckin.es6.js
+++ client/ClientCheckin.es6.js
@@ -9,37 +9,33 @@ var client = require('./client');
 
 class ClientCheckin extends ClientModule {
   constructor(options = {}) {
-    super('checkin', options.display || false);
+    super('checkin', true);
 
     this.index = null;
     this.label = null;
+    this.dialog = (options.dialog === true);
   }
 
   start() {
     super.start();
 
+    var contentDiv = document.createElement('div');
+    contentDiv.classList.add('centered-content');
+    this.view.appendChild(contentDiv);
+
     var socket = client.socket;
-    var contentDiv = null;
-
-    if (this.displayDiv) {
-      contentDiv = document.createElement('div');
-      contentDiv.classList.add('centered-content');
-      this.displayDiv.appendChild(contentDiv);
-    }
-
+    socket.emit('checkin_request');
 
     socket.on('checkin_info', (info) => {
       this.index = info.index;
       this.label = info.label;
 
-      if (contentDiv) {
+      if (this.dialog) {
         contentDiv.innerHTML = "<p>Go to position</p>" +
           "<div class='checkin-label circled'><span>" + this.label + "</span></div>" +
           "<p class='small'>Touch the screen<br/>when you are ready.</p>";
 
-        this.displayDiv.classList.remove('hidden');
-
-        this.displayDiv.addEventListener('click', () => {
+        this.view.addEventListener('click', () => {
           this.done();
         });
       } else {
@@ -48,13 +44,8 @@ class ClientCheckin extends ClientModule {
     });
 
     socket.on('checkin_failed', () => {
-      if (contentDiv) {
-        content.innerHTML = "<p>All seats are taken, please try again later! =)</p>";
-        this.displayDiv.classList.remove('hidden');
-      }
+      contentDiv.innerHTML = "<p>All seats are taken, please try again later.</p>";
     });
-
-    socket.emit('checkin_request');
   }
 }
 
