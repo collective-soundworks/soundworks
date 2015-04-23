ea9448560b24e74f4f145906f7dc511138263ba6
Norbert Schnell <Norbert.Schnell@ircam.fr>
2015-04-23 08:54:01 +0200

    moved (server-based) file list in separate module

--- client/ClientLoader.es6.js
+++ client/ClientLoader.es6.js
@@ -6,86 +6,109 @@
 
 var ClientModule = require('./ClientModule');
 var client = require('./client');
-var AudioBufferLoader = require('waves-loaders').AudioBufferLoader;
+var SuperLoader = require('waves-loaders').SuperLoader;
 
 class ClientLoader extends ClientModule {
   constructor(options = {}) {
     super(options.name || 'loader', true, options.color);
 
-    this._requestServer = options.server || false;
-    this._subfolder = options.subfolder || '';
-    this._audioFiles = options.audioFiles || null;
-    this._fileProgress = [];
+    this.extensions = options.extensions ||  ['.wav', '.mp3'];
 
-    this.audioBuffers = null;
+    this.files = options.files || null;
+    this.buffers = [];
 
-    let viewContent = document.createElement('div');
-    viewContent.classList.add('centered-content');
-    viewContent.classList.add('soft-blink');
-    this.view.appendChild(viewContent);
+    this._asynchronous = !!options.asynchronous;
 
-    let loadingText = document.createElement('p');
-    loadingText.innerHTML = "Loading sounds…";
-    viewContent.appendChild(loadingText);
+    this._fileProgress = null;
+    this._progressBar = null;
+    this._numFilesLoaded = 0;
 
-    let progressWrap = document.createElement('div');
-    progressWrap.classList.add('progress-wrap');
-    viewContent.appendChild(progressWrap);
+    if (!this._asynchronous) {
+      let viewContent = document.createElement('div');
+      viewContent.classList.add('centered-content');
+      viewContent.classList.add('soft-blink');
+      this.view.appendChild(viewContent);
 
-    let progressBar = document.createElement('div');
-    progressBar.classList.add('progress-bar');
-    progressWrap.appendChild(progressBar);
+      let loadingText = document.createElement('p');
+      loadingText.innerHTML = "Loading sounds…";
+      viewContent.appendChild(loadingText);
 
-    this.progressBar = progressBar;
+      let progressWrap = document.createElement('div');
+      progressWrap.classList.add('progress-wrap');
+      viewContent.appendChild(progressWrap);
+
+      let progressBar = document.createElement('div');
+      progressBar.classList.add('progress-bar');
+      progressWrap.appendChild(progressBar);
+
+      this._progressBar = progressBar;
+    }
   }
 
   start() {
     super.start();
-
-    if (this._requestServer) {
-      client.send('loader:request', this._subfolder);
-
-      client.receive('loader:files', (files) => {
-        let filePaths = [];
-
-        for (let i = 0; i < files.length; i++)
-          filePaths.push(this._subfolder + '/' + files[i]);
-        
-        this._audioFiles = filePaths;
-        this._load();
-      });
-    } else {
-      this._load();
-    }
+    this._load(this.files);
   }
 
-  _load() {
-    let loader = new AudioBufferLoader();
-    loader.progressCallback = this._progressCallback.bind(this);
+  _loadFile(index, file) {
+    let loader = new SuperLoader();
 
-    loader.load(this._audioFiles)
-      .then(
-        (audioBuffers) => {
-          this.audioBuffers = audioBuffers;
+    loader
+      .load([file])
+      .then((buffers) => {
+        let buffer = buffers[0];
+
+        this.buffers[index] = buffer;
+        this.emit('loader:fileLoaded', index, file, buffer);
+
+        this._numFilesLoaded++;
+        if (this._numFilesLoaded >= this.buffers.length)
+          this.emit('loader:allFilesLoaded');
+      }, (error) => {
+        console.log(error);
+      });
+  }
+
+  _load(fileList) {
+    if (this._asynchronous) {
+      for (let i = 0; i < fileList.length; i++)
+        this._loadFile(i, fileList[i]);
+
+      this.done();
+    } else {
+      let loader = new SuperLoader();
+
+      this._fileProgress = [];
+      
+      for(let i = 0; i < fileList.length; i++)
+        this._fileProgress[i] = 0;
+
+      loader.progressCallback = this._progressCallback.bind(this);
+      loader.load(fileList)
+        .then((buffers) => {
+          this.buffers = buffers;
+          this.emit('loader:allFilesLoaded');
           this.done();
         }, (error) => {
           console.log(error);
-        }
-      );
+        });
+    }
   }
 
   _progressCallback(obj) {
     let progress = 0;
-    this._fileProgress[obj.index] = obj.value;
+    let fileIndex = obj.index;
+    let fileProgress = obj.value;
+
+    this._fileProgress[fileIndex] = fileProgress;
 
     for (let i = 0; i < this._fileProgress.length; i++) {
-      progress += this._fileProgress[i] / this._audioFiles.length;
+      progress += this._fileProgress[i] / this._fileProgress.length;
     }
 
     progress = Math.ceil(progress * 100);
-    this.progressBar.style.width = progress + "%";
+    this._progressBar.style.width = progress + "%";
   }
-
 }
 
 module.exports = ClientLoader;
