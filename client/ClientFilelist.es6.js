dc10973d122b4c78b9080e263478bcceb05928e7
Norbert Schnell <Norbert.Schnell@ircam.fr>
2015-04-23 08:56:05 +0200

    adde new client/server FileList module

---
+++ client/ClientFilelist.es6.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var client = require('./client');
+var ClientModule = require('./ClientModule');
+
+class Filelist extends ClientModule {
+  constructor(options = {}) {
+    super(options.name || 'filelist', false);
+
+    this.folder = options.folder || '';
+    this.extensions = options.extensions || undefined;
+    this.files = null;
+  }
+
+  start() {
+    super.start();
+
+    client.send('filelist:request', this.folder, this.extensions);
+    client.receive('filelist:files', (files) => {
+      this.files = files;
+      this.emit('filelist:files', files);
+      this.done();
+    }, this);
+  }
+}
+
+module.exports = Filelist;
