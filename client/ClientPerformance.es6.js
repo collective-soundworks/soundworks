8061fea2ffb13e930c99816e7cc8c780e46de211
Norbert Schnell <Norbert.Schnell@ircam.fr>
2015-05-21 11:40:02 +0200

    (re-)removed master argument

--- client/ClientPerformance.es6.js
+++ client/ClientPerformance.es6.js
@@ -8,23 +8,17 @@ var ClientModule = require('./ClientModule');
 var client = require('./client');
 
 class ClientPerformance extends ClientModule {
-  constructor(name = 'performance', master = true, viewColor = 'black') {
+  constructor(name = 'performance', viewColor = 'black') {
     super(name, true, viewColor);
-
-    this.master = master;
   }
 
   start() {
     super.start();
-
-    if(this.master)
-      client.send('performance:start');
+    client.send('performance:start');
   }
 
   done() {
-    if(this.master)
-      client.send('performance:done');
-
+    client.send('performance:done');
     super.done();
   }
 }
