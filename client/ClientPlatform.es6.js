227c056bd5b2ffa2fe4667c2b382c3e3da722048
b-ma <benjamin.matuszewski@gmail.com>
2015-06-01 16:27:13 +0200

    revised platform check

--- client/ClientPlatform.es6.js
+++ client/ClientPlatform.es6.js
@@ -4,28 +4,30 @@
  */
 'use strict';
 
-var client = require('./client');
+// var client = require('./client');
 var ClientModule = require('./ClientModule');
-var platform = require('platform');
+// var platform = require('platform');
+const MobileDetect = require('mobile-detect');
+const audioContext = require('waves-audio').audioContext;
 
-function parseVersionString(string) {
-  if (string) {
-    var a = string.split('.');
+// function parseVersionString(string) {
+//   if (string) {
+//     var a = string.split('.');
 
-    if (a[1] >= 0)
-      return parseFloat(a[0] + "." + a[1]);
+//     if (a[1] >= 0)
+//       return parseFloat(a[0] + "." + a[1]);
 
-    return parseFloat(a[0]);
-  }
+//     return parseFloat(a[0]);
+//   }
 
-  return null;
-}
+//   return null;
+// }
 
 var defaultMessages = {
   iosVersion: "This application requires at least iOS 7.",
   androidVersion: "This application requires at least Android 4.2.",
-  useChrome: "You have to use Chrome to run this application on an Android device.",
-  updateChrome: "Please update Chrome to a more recent version to run this application.",
+  // useChrome: "You have to use Chrome to run this application on an Android device.", // not used
+  // updateChrome: "Please update Chrome to a more recent version to run this application.", // not used
   wrongOS: "This application is designed for iOS and Android mobile devices."
 };
 
@@ -36,29 +38,57 @@ class ClientPlatform extends ClientModule {
     this.prefix = options.prefix ||  '';
     this.postfix = options.postfix ||  '';
     this.messages = options.messages || defaultMessages;
+
+    this.md = new MobileDetect(window.navigator.userAgent);
   }
 
   start() {
     super.start();
 
-    var osVersion = parseVersionString(platform.os.version);
-    var browserVersion = parseVersionString(platform.version);
+    this.isMobile = (this.md.mobile() !== null); // true if phone or tablet
+    this.os = (() => {
+      if (this.md.os() === 'AndroidOS') {
+        return 'android';
+      } else if (this.md.os() === 'iOS') {
+        return 'ios';
+      } else {
+        return 'not supported';
+      }
+    })();
+
+    // var osVersion = parseVersionString(platform.os.version);
+    // var browserVersion = parseVersionString(platform.version);
     var msg = null;
 
-    if (!client.audioContext) {
-      if (platform.os.family == "iOS") {
-        if (osVersion < 7)
-          msg = this.messages.iosVersion;
-      } else if (platform.os.family == "Android") {
-        if (osVersion < 4.2)
-          msg = this.messages.androidVersion;
-        else if (platform.name != 'Chrome Mobile')
-          msg = this.messages.useChrome;
-        else if (browserVersion < 35)
-          msg = this.messages.updateChrome;
+    // if (!client.audioContext) {
+    //   if (platform.os.family == "iOS") {
+    //     if (osVersion < 7)
+    //       msg = this.messages.iosVersion;
+    //   } else if (platform.os.family == "Android") {
+    //     if (osVersion < 4.2)
+    //       msg = this.messages.androidVersion;
+    //     else if (platform.name != 'Chrome Mobile')
+    //       msg = this.messages.useChrome;
+    //     else if (browserVersion < 35)
+    //       msg = this.messages.updateChrome;
+    //   } else {
+    //     msg = this.messages.wrongOS;
+    //   }
+    // }
+    // console.log(audioContext);
+    // console.log(this.os);
+    // console.log(this.isMobile);
+
+    if (!audioContext) {
+      if (this.os === 'ios') {
+        msg = this.messages.iosVersion;
+      } else if (this.os === 'android') {
+        msg = this.messages.androidVersion;
       } else {
         msg = this.messages.wrongOS;
       }
+    } else if (!this.isMobile) {
+      msg = this.messages.wrongOS;
     }
 
     if (msg !== null) {
