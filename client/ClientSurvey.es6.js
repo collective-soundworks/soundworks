88c5d938e784997cd84d427acb0abb685f1d7307
b-ma <benjamin.matuszewski@gmail.com>
2015-10-01 18:35:16 +0200

    updated Survey

--- client/ClientSurvey.es6.js
+++ client/ClientSurvey.es6.js
@@ -1,16 +1,37 @@
 const ClientModule = require('./ClientModule');
 const client = require('./client');
 
+
+class BaseRenderer {
+  constructor(survey, question) {
+    this.survey = survey;
+    this.question = question;
+    this.label = question.label;
+    this.id = question.id;
+  }
+
+  render() {  return ''; }
+
+  destroy() {
+    this._unbindEvents();
+  }
+
+  bindEvents() {}
+  _unbindEvents() {}
+
+  getAnswer() {}
+}
+
 // renderers
-class AbstractSelectorQuestion {
-    constructor(label, answers) {
-    this.label = label;
-    this.answers = answers;
+class AbstractSelectorRenderer extends BaseRenderer {
+  constructor(survey, question) {
+    super(survey, question);
+    this.answers = question.answers;
 
     this._onSelect = this._onSelect.bind(this);
   }
 
-  render(container) {
+  render(container, type) {
     this.container = container;
 
     let title = `<p class="label">${this.label}</p>`;
@@ -18,14 +39,10 @@ class AbstractSelectorQuestion {
 
     for (let key in this.answers) {
       let value = this.answers[key];
-      answers += `<p class="answer ${this.type}" data-key="${key}">${value}</p>`;
+      answers += `<p class="answer ${type}" data-key="${key}">${value}</p>`;
     }
 
     return `<div>${title}${answers}</div>`;
-  }
-
-  destroy() {
-    this.unbindEvents();
   }
 
   bindEvents() {
@@ -33,15 +50,18 @@ class AbstractSelectorQuestion {
     this.container.addEventListener('click', this._onSelect, false);
   }
 
-  unbindEvents() {
+  _unbindEvents() {
     this.container.removeEventListener('click', this._onSelect, false);
   }
 }
 
-class RadioQuestion extends AbstractSelectorQuestion {
-  constructor(label, answers) {
-    super(label, answers);
-    this.type = 'radio'
+class RadioRenderer extends AbstractSelectorRenderer {
+  constructor(survey, question) {
+    super(survey, question);
+  }
+
+  render(container) {
+    return super.render(container, 'radio');
   }
 
   _onSelect(e) {
@@ -50,6 +70,8 @@ class RadioQuestion extends AbstractSelectorQuestion {
 
     this.answersEl.forEach((el) => { el.classList.remove('selected'); });
     target.classList.add('selected');
+
+    this.survey.enableBtn();
   }
 
   getAnswer() {
@@ -64,10 +86,13 @@ class RadioQuestion extends AbstractSelectorQuestion {
   }
 }
 
-class CheckboxQuestion extends AbstractSelectorQuestion {
-  constructor(label, answers) {
-    super(label, answers);
-    this.type = 'checkbox'
+class CheckboxRenderer extends AbstractSelectorRenderer {
+  constructor(survey, question) {
+    super(survey, question);
+  }
+
+  render(container) {
+    return super.render(container, 'checkbox');
   }
 
   _onSelect(e) {
@@ -76,6 +101,8 @@ class CheckboxQuestion extends AbstractSelectorQuestion {
 
     const method = target.classList.contains('selected') ? 'remove' : 'add';
     target.classList[method]('selected');
+
+    this.survey.enableBtn();
   }
 
   getAnswer() {
@@ -92,13 +119,14 @@ class CheckboxQuestion extends AbstractSelectorQuestion {
   }
 }
 
-class RangeQuestion {
-  constructor(label, min = 0, max = 10, step = 1, defaultValue = 5) {
-    this.label = label;
-    this.min = min;
-    this.max = max;
-    this.step = step;
-    this.defaultValue = defaultValue;
+class RangeRenderer extends BaseRenderer {
+  constructor(survey, question) {
+    super(survey, question);
+    this.label = question.label;
+    this.min = question.min === undefined ? 0 : question.min;
+    this.max = question.max === undefined ? 10 : question.max;
+    this.step = question.step === undefined ? 1 : question.step;
+    this.defaultValue = question.defaultValue === undefined ? 5 : question.defaultValue;
 
     this._onInput = this._onInput.bind(this);
   }
@@ -119,7 +147,7 @@ class RangeQuestion {
     this._range.classList.add('slider', 'answer');
 
     this._number = document.createElement('span');
-    this._number.classList.add('feedback');
+    this._number.classList.add('range-feedback');
     this._number.textContent = this.defaultValue;
 
     const div = document.createElement('div');
@@ -130,24 +158,44 @@ class RangeQuestion {
     return div;
   }
 
-  destroy() {
-    this.unbindEvents();
-  }
-
   bindEvents() {
     this._range.addEventListener('input', this._onInput, false);
   }
 
-  unbindEvents() {
+  _unbindEvents() {
     this._range.removeEventListener('input', this._onInput, false);
   }
 
   _onInput(e) {
     this._number.textContent = this._range.value;
+
+    this.survey.enableBtn();
   }
 
   getAnswer() {
     return parseFloat(this._range.value);
+  }
+}
+
+// is never required for now
+class TextAreaRenderer extends BaseRenderer {
+  constructor(survey, question) {
+    super(survey, question);
+    this.label = question.label;
+  }
+
+  render(container) {
+    this._container = container;
+
+    return `
+      <p class="label">${this.label}</p>
+      <textarea class="answer textarea"></textarea>
+    `;
+  }
+
+  getAnswer() {
+    const textarea = this._container.querySelector('.answer');
+    return textarea.value;
   }
 }
 
@@ -156,32 +204,37 @@ class ClientSurvey extends ClientModule {
   constructor(surveyConfig, options = {}) {
     super(options.name || 'survey', true, options.color);
 
-    if (surveyConfig.thanksContent === undefined) {
-      surveyConfig.thanksContent = '<p class="thanks">Thanks</p>';
+    if (options.thanks === undefined) {
+      options.thanks = 'Thanks';
     }
 
+    options.thanksContent = `<p class="thanks">${options.thanks}</p>`;
+    options.btnNextText = options.btnNextText || 'next';
+    options.btnValidateText = options.btnValidateText || 'validate';
+
     this.survey = surveyConfig;
+    this.options = options;
     this.contents = [];
     this.answers = {};
+    this.questionCounter = 0;
 
-    this.createRenderers();
-    this.currentQuestionIndex = 0;
-    this.render();
+    this._createRenderers();
+    this._render();
 
-    this.displayNextQuestion = this.displayNextQuestion.bind(this);
+    this._displayNextQuestion = this._displayNextQuestion.bind(this);
 
-    this.displayNextQuestion();
-    this.bindEvents();
+    this._displayNextQuestion();
+    this._bindEvents();
   }
 
-  render() {
+  _render() {
     const counter = document.createElement('div');
     counter.classList.add('counter');
     counter.innerHTML = `<span></span> / ${this.survey.length}`;
 
     const nextBtn = document.createElement('button');
     nextBtn.classList.add('next', 'btn');
-    nextBtn.textContent = 'next';
+    nextBtn.textContent = this.options.btnNextText; // @TODO option
 
     this.view.appendChild(counter);
     this.view.appendChild(nextBtn);
@@ -191,75 +244,90 @@ class ClientSurvey extends ClientModule {
     this._nextBtn = nextBtn;
   }
 
-  bindEvents() {
-    this._nextBtn.addEventListener('click', this.displayNextQuestion, false);
+  _bindEvents() {
+    this._nextBtn.addEventListener('click', this._displayNextQuestion, false);
   }
 
-  createRenderers() {
-    this.renderers = this.survey.map((q, index) => {
-      let renderer;
+  _unbindEvents() {
+    this._nextBtn.removeEventListener('click', this._displayNextQuestion, false);
+  }
 
-      switch (q.type) {
+  _createRenderers() {
+    this.renderers = this.survey.map((question, index) => {
+      question.required = question.required === undefined ? true : question.required;
+      question.id = question.id || `question-${index}`;
+
+      let ctor;
+
+      switch (question.type) {
         case 'radio':
-          renderer = new RadioQuestion(q.label, q.answers);
+          ctor = RadioRenderer;
           break;
         case 'checkbox':
-          renderer = new CheckboxQuestion(q.label, q.answers);
+          ctor = CheckboxRenderer;
           break;
         case 'range':
-          renderer = new RangeQuestion(q.label, q.min, q.max, q.step, q.defaultValue);
+          ctor = RangeRenderer;
+          break;
+        case 'textarea':
+          question.required = false;
+          ctor = TextAreaRenderer;
           break;
       }
 
-      renderer.id = q.id || `question-${index}`;
-      return renderer;
+      return new ctor(this, question);
     });
   }
 
-  displayNextQuestion() {
+  _displayNextQuestion() {
+    // handle current question if any
     if (this.currentRenderer) {
       const answer = this.currentRenderer.getAnswer();
-      if (answer === null) { return; }
+      const required = this.currentRenderer.question.required;
 
-      this.answers[this.currentRenderer.id] = answer;
+      if (answer === null && required) { return; }
+
       this.currentRenderer.destroy();
+      this.answers[this.currentRenderer.id] = answer;
     }
 
-    this.currentRenderer = this.renderers[this.currentQuestionIndex];
+    this.currentRenderer = this.renderers.shift();
+
     if (this.currentRenderer) {
       // update counter
-      this._currentQuestionCounter.textContent = this.currentQuestionIndex + 1;
+      this.questionCounter += 1;
+      this._currentQuestionCounter.textContent = this.questionCounter;
+      // update content
       const htmlContent = this.currentRenderer.render(this.view);
       this.setCenteredViewContent(htmlContent);
       this.currentRenderer.bindEvents();
 
-      this.currentQuestionIndex += 1;
+      if (this.currentRenderer.question.required) {
+        this.disableBtn();
+      }
+
+      if (this.renderers.length === 0) {
+        this._nextBtn.textContent = this.options.btnValidateText;
+      }
     } else {
       // remove counter and next buttons
       this.view.removeChild(this._counter);
       this.view.removeChild(this._nextBtn);
       // display thanks
-      this.setCenteredViewContent(this.survey.thanksContent);
+      this.setCenteredViewContent(this.options.thanksContent);
       // send informations to server
       this.answers.timestamp = new Date().getTime();
       client.send(`${this.name}:answers`, JSON.stringify(this.answers));
-      // this.done(); // when should we call this ?
     }
   }
 
-  start() {
-    super.start();
+  disableBtn() {
+    this._nextBtn.classList.add('disabled');
   }
 
-  restart() {
-    super.restart();
+  enableBtn() {
+    this._nextBtn.classList.remove('disabled');
   }
-
-  reset() {
-    super.reset();
-  }
-
-
 }
 
 module.exports = ClientSurvey;
