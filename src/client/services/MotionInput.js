33c4c8c36736a7b3f774286f8e1f540fa687ef38
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-02-02 16:31:14 +0100

    updated motionInput API and documentation

--- src/client/services/MotionInput.js
+++ src/client/services/MotionInput.js
@@ -4,36 +4,46 @@ import serviceManager from '../core/serviceManager';
 
 const SERVICE_ID = 'service:motion-input';
 
-/*
-... dans le constructor (ou la méthode init ?) :
-  this.motionInput = this.require(‘motioninput', { descriptors: [‘accelerationIncludingGravity'] });
 
-… dans le start/enter de l'experience:
-  if (motionInput.isValid(‘accelerationIncludingGravity')) {
-    motionInput.addListener('accelerationIncludingGravity', (data) => {
-      // digest motion data
-    });
-  } else {
-    // handle error
-  }
-*/
-
+/**
+ * Wrapper for the motion-input module.
+ *
+ * @example
+ * // in the experince constructor
+ * this.motionInput = this.require(‘motioninput', {
+ *   descriptors: [‘accelerationIncludingGravity']
+ * });
+ *
+ * // in the experience start
+ * if (this.motionInput.isAvailable(‘accelerationIncludingGravity')) {
+ *   this.motionInput.addListener('accelerationIncludingGravity', (data) => {
+ *     // digest motion data
+ *   });
+ * } else {
+ *   // handle error
+ * }
+ */
 class MotionInput extends Service {
   constructor() {
     super(SERVICE_ID, false);
 
     const defaults = {
       descriptors: [],
-      // showError: false, // @todo - how to handle if several descriptors are asked but only some passed ? showAnyError / showAllError
+      // @todo - how to handle if only descriptors are invalid ?
+      // showError: false,
     };
 
     this.configure(defaults);
-    // @todo - handle directly inside the motionInput
+    // @todo - should be handled directly inside the motionInput
     this._descriptorsValidity = {}
   }
 
   // init() { /* nothing to do here... */ }
 
+  /**
+   * Override default configure to add descriptors from multiple calls.
+   * @param {Object} options - The options to apply to the service.
+   */
   configure(options) {
     if (this.options.descriptors)
       options.descriptors = this.options.descriptors.concat(options.descriptors);
@@ -41,6 +51,7 @@ class MotionInput extends Service {
     super.configure(options);
   }
 
+  /** @private */
   start() {
     super.start();
 
@@ -56,19 +67,35 @@ class MotionInput extends Service {
       });
   }
 
+  /** @private */
   stop() {
     super.stop();
   }
 
-  isValid(name) {
+  /**
+   * Define if a given descriptor is available or not
+   * @param {String} name - The descriptor name.
+   * @returns {Boolean}
+   */
+  isAvailable(name) {
     return this._descriptorsValidity[name];
   }
 
+  /**
+   * Add a listener to a given descriptor.
+   * @param {String} name - The descriptor name.
+   * @param {Function} callback - The callback to register.
+   */
   addListener(name, callback) {
     if (this._descriptorsValidity[name])
       motionInput.addListener(name, callback);
   }
 
+  /**
+   * Remove a listener from a given descriptor.
+   * @param {String} name - The descriptor name.
+   * @param {Function} callback - The callback to remove.
+   */
   removeListener(name, callback) {
     if (this._descriptorsValidity[name])
       motionInput.removeListener(name, callback);
