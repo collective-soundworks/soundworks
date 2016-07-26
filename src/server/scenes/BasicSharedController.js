cd73c757eebbe51e6226159671429a4ebbab3f90
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-07-26 11:44:23 +0200

    added `service:error-reporter` as dependency

--- src/server/scenes/BasicSharedController.js
+++ src/server/scenes/BasicSharedController.js
@@ -6,6 +6,8 @@ export default class BasicSharedController extends Scene {
   constructor(clientType) {
     super(SCENE_ID, clientType);
 
+    this._errorReporter = this.require('error-reporter');
+
     /**
      * Instance of the server-side `shared-params` service.
      * @type {module:soundworks/server.SharedParams}
