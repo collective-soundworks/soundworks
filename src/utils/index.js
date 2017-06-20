34eda2cbbb6cbabb0617d483f39fefe0f5de64fe
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2017-06-20 15:13:17 +0200

    exposed utils properly

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,10 @@
-console.log('entry point for `utils`');
+import * as _helpers from './helpers';
+import * as _math from './math';
+import * as _setup from './setup';
+
+export { default as EventEmitter } from './EventEmitter';
+export { default as Signal } from './Signal';
+export { default as SignalAll } from './SignalAll';
+export const helpers = _helpers;
+export const math = _math;
+export const setup = _setup;
