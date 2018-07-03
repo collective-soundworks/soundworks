3ea1c5937fc5552a797eb61a5c6620966aa75ad9
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2018-07-03 19:17:12 +0200

    added deprecation warning

--- src/server/prefabs/ControllerExperience.js
+++ src/server/prefabs/ControllerExperience.js
@@ -3,11 +3,14 @@ import Experience from '../core/Experience';
 /**
  * Server-side experience to create 1 liner controllers
  *
+ * @deprecated
  * @memberof module:soundworks/server
  */
 class ControllerExperience extends Experience {
   constructor(clientTypes, options = {}) {
     super(clientTypes);
+
+    console.error('[deprecated] ControllerExperience is deprecated, has moved in soundworks-template and will be removed in soundworks#v3.0.0. Please consider updating your application from soundworks-template');
 
     /**
      * Instance of the server-side `shared-params` service.
