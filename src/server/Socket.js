e0605773ff9c7929ef6baf4ceb8676c047fc6a14
b-ma <benjamin.matuszewski@gmail.com>
2021-06-07 17:36:56 +0200

    check socket readyState before sending message

--- src/server/Socket.js
+++ src/server/Socket.js
@@ -251,11 +251,17 @@ class Socket {
   send(channel, ...args) {
     const msg = packStringMessage(channel, ...args);
 
-    this.ws.send(msg, (err) => {
-      if (err) {
-        console.error('error sending msg:', channel, args, err);
-      }
-    });
+    // 0 CONNECTING  Socket has been created. The connection is not yet open.
+    // 1 OPEN  The connection is open and ready to communicate.
+    // 2 CLOSING The connection is in the process of closing.
+    // 3 CLOSED  The connection is closed or couldn't be opened.
+    if (this.ws.readyState === 1) {
+      this.ws.send(msg, (err) => {
+        if (err) {
+          console.error('error sending msg:', channel, args, err);
+        }
+      });
+    }
   }
 
   /**
