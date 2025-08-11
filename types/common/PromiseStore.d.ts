56faaebb094a96e29c2b015ec666be37c2d983c8
b-ma <benjamin.matuszewski@gmail.com>
2025-08-11 17:07:33 +0200

    5.2.1

--- types/common/PromiseStore.d.ts
+++ types/common/PromiseStore.d.ts
@@ -3,9 +3,13 @@ export default class PromiseStore {
     constructor(name: any);
     name: any;
     store: Map<any, any>;
-    generateRequestId: any;
-    add(resolve: any, reject: any, type: any, localParams: any): any;
-    resolve(reqId: any, data: any): void;
-    reject(reqId: any, msg: any): void;
+    generateId: any;
+    createPromise(): {
+        id: any;
+        promise: any;
+    };
+    associateResolveData(id: any, associatedData: any): void;
+    resolve(id: any, data: any): void;
+    reject(id: any, msg: any): void;
     flush(): void;
 }
--- types/common/version.d.ts
+++ types/common/version.d.ts
@@ -1,2 +1,2 @@
-declare const _default: "5.2.0";
+declare const _default: "5.2.1";
 export default _default;
